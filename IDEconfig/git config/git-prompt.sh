e7e68e75753ba6c1eec83ec00998686f5bb2bca2
dreschpedro <99pedrodresch@gmail.com>
2022-08-15 17:15:48 -0300

    change chocolatey file

--- IDEconfig/git config/git-prompt.sh
+++ IDEconfig/git config/git-prompt.sh
@@ -10,8 +10,8 @@ then
 	. ~/.config/git/git-prompt.sh
 else
 	PS1='\[\033]0;$PWD\007\]' # set window title
-	#PS1="$PS1"'\n'                 # new line
-	#PS1="$PS1"'\[\033[32m\]'       # change to green
+	PS1="$PS1"'\n'                 # new line
+	PS1="$PS1"'\[\033[32m\]'       # change to green
 	#PS1="$PS1"'\u@\h '             # user@host<space>
 	#PS1="$PS1"'\[\033[35m\]'       # change to purple
 	#PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
