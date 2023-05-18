f5f8fa47f76970d16fad16c89710aee8309e9ec4
dreschpedro <99pedrodresch@gmail.com>
2023-05-18 00:35:50 -0300

    muchas cosas

---
+++ IDEconfig/git_config/aliases.sh
@@ -0,0 +1,35 @@
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
+
+# --show-control-chars: help showing Korean or accented characters
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+alias cls='clear'
+alias cll='clear'
+alias gst='git status'
+alias gpo='git push origin'
+alias gplo='git pull origin'
+alias gcm='git commit -m'
+alias gad='git add .'
+alias gp='git push'
+alias gpl='git pull'
+alias ..='cd ..'
+alias gcun='git config --global user.name "dreschpedro"'
+alias gcue='git config --global user.email "99pedrodresch@gmail.com"'
+alias aguf='sudo apt update && apt full-upgrade -y'
+alias abrir='xdg-open'
+
+case "$TERM" in
+xterm*)
+	# The following programs are known to require a Win32 Console
+	# for interactive usage, therefore let's launch them through winpty
+	# when run inside `mintty`.
+	for name in node ipython php php5 psql python2.7
+	do
+		case "$(type -p "$name".exe 2>/dev/null)" in
+		''|/usr/bin/*) continue;;
+		esac
+		alias $name="winpty $name.exe"
+	done
+	;;
+esac
